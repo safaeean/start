43c4862aa623263513e8dd038c6628a61325f97d
hossein <info@mndco.ir>
2023-06-09 01:31:40 +0330

    laravel passport api

--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => ['cors', 'json.response'],'prefix' => 'api'], function () {
+
+    // ...
+
+    // public routes
+    Route::post('/login', 'Auth\ApiAuthController@login')->name('login');
+    Route::post('/register','Auth\ApiAuthController@register')->name('register');
+    Route::post('/logout', 'Auth\ApiAuthController@logout')->name('logout');
+
+    // ...
+
 });

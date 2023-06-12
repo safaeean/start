7a7f40f5fa5853dfa919fc689b0f8b5f046ae16b
hossein <info@mndco.ir>
2023-06-13 00:03:58 +0330

    Add client only and fix lint

--- Frontend/components/Layouts/HeaderComponent.vue
+++ Frontend/components/Layouts/HeaderComponent.vue
@@ -1,37 +1,49 @@
 <template>
   <header id="header">
-    <nav id="main-nav" class="navbar navbar-default navbar-fixed-top" role="banner">
+    <nav id="main-nav" class="navbar navbar-default navbar-fixed-top">
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
             <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
+            <span class="icon-bar" />
+            <span class="icon-bar" />
+            <span class="icon-bar" />
           </button>
-          <NuxtLink class="navbar-brand" to="/"><img src="~/assets/images/logo.png" alt="logo"></NuxtLink>
+          <NuxtLink class="navbar-brand" to="/">
+            <img src="~/assets/images/logo.png" alt="logo">
+          </NuxtLink>
         </div>
 
         <div class="collapse navbar-collapse navbar-right">
           <ul class="nav navbar-nav">
-            <li><NuxtLink to="/">Home</NuxtLink></li>
-            <li v-if="!$auth.user"><NuxtLink to="/auth/register">Register</NuxtLink></li>
-            <li v-if="!$auth.user"><NuxtLink to="/auth/login">Login</NuxtLink></li>
-            <li v-if="user"><NuxtLink to="/profile">{{ user.name }}</NuxtLink></li>
+            <client-only>
+              <li>
+                <NuxtLink to="/">
+                  Home
+                </NuxtLink>
+              </li>
+              <li v-if="!$auth.loggedIn">
+                <NuxtLink to="/auth/register">
+                  Register
+                </NuxtLink>
+              </li>
+              <li v-if="!$auth.loggedIn">
+                <NuxtLink to="/auth/login">
+                  Login
+                </NuxtLink>
+              </li>
+              <li v-if="$auth.loggedIn">
+                <NuxtLink to="/profile">
+                  {{ $auth.$state.user.name }}
+                </NuxtLink>
+              </li>
+            </client-only>
           </ul>
         </div>
       </div><!--/.container-->
     </nav><!--/nav-->
   </header><!--/header-->
 </template>
-<style>
-.collapse {
-  visibility: inherit!important;
-}
-</style>
-<style>
-@import "@/assets/css/main.css";
-</style>
 <script>
 export default {
   data () {
@@ -41,3 +53,11 @@ export default {
   }
 }
 </script>
+<style>
+.collapse {
+  visibility: inherit!important;
+}
+</style>
+<style>
+@import "@/assets/css/main.css";
+</style>
--- Frontend/pages/profile.vue
+++ Frontend/pages/profile.vue
@@ -1,18 +1,20 @@
 <template>
   <section id="services">
-    <div class="container" v-if="user">
-      <div class="form-group">
-        <label for="name">Name :</label>
-        <input type="text" class="form-control" id="name" v-model="user.name" required>
+    <client-only>
+      <div class="container" v-if="user">
+        <div class="form-group">
+          <label for="name">Name :</label>
+          <input type="text" class="form-control" id="name" v-model="user.name" required>
+        </div>
+        <div class="form-group">
+          <label for="email">Email :</label>
+          <input type="email" class="form-control" id="email" v-model="user.email" required>
+        </div>
+        <div class="form-group">
+          <button type="submit" class="btn btn-success">Register</button>
+        </div>
       </div>
-      <div class="form-group">
-        <label for="email">Email :</label>
-        <input type="email" class="form-control" id="email" v-model="user.email" required>
-      </div>
-      <div class="form-group">
-        <button type="submit" class="btn btn-success">Register</button>
-      </div>
-    </div>
+    </client-only>
   </section>
 </template>
 

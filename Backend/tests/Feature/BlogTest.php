75b1c1c0b195229cb4b135db90856f047d78f865
hossein <info@mndco.ir>
2023-06-26 03:14:17 +0330

    Blog api test

---
+++ Backend/tests/Feature/BlogTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Blog\Post;
+use Tests\TestCase;
+
+class BlogTest extends TestCase
+{
+    /**
+     * Test the blog posts api returns a successful response.
+     */
+    public function test_the_blog_posts_api_returns_a_successful_response(): void
+    {
+        $response = $this->get('/api/blog/posts');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test the blog post api returns a successful response.
+     */
+    public function test_the_blog_post_api_returns_a_successful_response(): void
+    {
+        $post = Post::query()->first();
+        $response = $this->get("/api/blog/post/{$post->id}");
+        $response->assertStatus(200);
+    }
+}

46a43d6c5874904b0ea31fab657c7b057332df03
hossein <info@mndco.ir>
2023-06-26 03:11:26 +0330

    Post factory

---
+++ Backend/database/factories/Blog/PostFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories\Blog;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->name(),
+            'user_id' => User::query()->inRandomOrder()->first()->id,
+            'description' => fake()->text(),
+            'content' => fake()->text(),
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     */
+    public function unverified(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
+        ]);
+    }
+}

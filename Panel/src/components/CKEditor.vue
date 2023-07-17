89fff657baf7a7128753dd7b0a6c59edd66b8430
hossein <info@mndco.ir>
2023-07-18 02:18:43 +0330

    CKEditor component

---
+++ Panel/src/components/CKEditor.vue
@@ -0,0 +1,46 @@
+<template>
+  <ckeditor :editor="editor" v-model="localValue" :config="editorConfig" @ready="onReady"
+            @input="onChange"></ckeditor>
+</template>
+<script>
+
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import CKEditor from "@ckeditor/ckeditor5-vue"
+
+export default {
+  props: {
+    value: String,
+    lang: String
+  },
+  name: 'ck-editor',
+  components: {
+    ckeditor: CKEditor.component
+  },
+  data() {
+    return {
+      editor: ClassicEditor,
+      editorConfig: {
+        language: this.lang,
+        defaultLanguage: this.lang,
+        height: '25em',
+      },
+      localValue: this.value
+    };
+  },
+  methods: {
+    onReady() {
+      setTimeout(() => {
+        this.localValue = this.value
+      }, 600)
+    },
+    onChange(data) {
+      this.$emit('change', {data: data});
+    },
+  }
+}
+</script>
+<style>
+.ck.ck-content {
+  min-height: 300px;
+}
+</style>

3c6b8514057a71d70973c1edbcd8d6a222bd19b9
hossein <info@mndco.ir>
2023-07-21 02:13:07 +0330

    Bootstrap js

--- Frontend/nuxt.config.js
+++ Frontend/nuxt.config.js
@@ -21,6 +21,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    {src: '~/plugins/bootstrap.js', mode: 'client'}
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
